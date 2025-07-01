1304e69e8638f09a8f77dac5043a434742d72eb9
kenechukwuJosiah <onwe.josiah@appmartgroup.com>
2025-07-01 01:04:35 +0100

    feat: implement JWT authentication and user login functionality

--- apps/backend/auth/src/app.controller.ts
+++ apps/backend/auth/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AppService) {}
 
   @Get('ping')
   getPing() {
-    return this.appService.getPing();
+    return this.authService.getPing();
+  }
+
+  @Post('login')
+  async login(
+    @Body() body: { email: string; password: string; role: 'user' | 'admin' },
+  ) {
+    const user = await this.authService.validateUser(
+      body.email,
+      body.password,
+      body.role,
+    );
+    return this.authService.login(user);
   }
 }
